c414bb36103adff8213171c9c8b6c344b980445d
Diego Di Palma <diegodipalma@outlook.com>
2018-01-25 18:34:49 +0100

    Aggiungere i file di progetto.

---
+++ tutorial2/SelezionaCompetenze.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace tutorial2
+{
+    public partial class SelezionaCompetenze : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void lstCompetenze_DataBound(object sender, EventArgs e)
+        {
+            ListBox lstCompetenze = (ListBox)sender;
+            string competenza;
+            foreach (object c in competenzexstudenti.Select(DataSourceSelectArguments.Empty))
+            {
+                competenza = DataBinder.Eval(c, "Competenza").ToString();
+                for (int i = 0; i < lstCompetenze.Items.Count; i++)
+                {
+                    if (competenza == lstCompetenze.Items[i].Text)
+                    {
+                        lstCompetenze.Items[i].Selected = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
